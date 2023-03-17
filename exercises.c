aadbd3cae7811d7a6ebb16c219dcc61912dd69a3
Your Name <you@example.com>
2023-03-16 22:13:39 -0300

    new try

--- exercises.c
+++ exercises.c
@@ -26,11 +26,11 @@ almacena en las variables apuntadas por max y min.
 */
 void arrayMaxMin(int *a, int n, int *max, int *min) {
    for(int i = 0; i < n; i++) {
-      if(a[i] > *max) {
-         *max = a[i];
+      if(a[i] > (*max)) {
+         (*max) = a[i];
       }
-      if(a[i] < *min) {
-         *min = a[i];
+      if(a[i] < (*min)) {
+         (*min) = a[i];
       }
    }
 }
